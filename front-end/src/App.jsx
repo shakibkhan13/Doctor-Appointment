1a7377a0793a2409d90a2370356810fa4e57c97f
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-11-23 08:18:54 +0600

    Shakib_Dev

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import MyAppointment from './pages/MyAppointment'
+import Doctors from './pages/Doctors'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import Login from './pages/Login'
+import Appointment from './pages/Appointment'
+import Navbar from './Components/Navbar'
+
 
 
 function App() {
   return (
-    <div className='text-green-500'>
-      <h1>Shakib</h1>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/myappointment' element={<MyAppointment />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/profile' element={<MyProfile />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+
+
     </div>
   )
 }
---
+++ front-end/src/pages/MyAppointment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyAppointment = () => {
+  return (
+    <div>
+      appointment
+    </div>
+  )
+}
+
+export default MyAppointment
