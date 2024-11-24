ba84aea545d624846ac900f7e2c030533056e49d
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-11-24 15:18:17 +0600

    Shakib_dev

--- front-end/src/Components/TopDoctors.jsx
+++ front-end/src/Components/TopDoctors.jsx
@@ -19,7 +19,6 @@ const TopDoctors = () => {
                     <p className='text-gray-600 text-sm'>{item.speciality}</p>
                 </div>
             </div>
-
         ))}
       </div>
       <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
