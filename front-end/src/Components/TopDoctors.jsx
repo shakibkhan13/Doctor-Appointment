ece6ec3ee66948f8fd72f224058ce6199c680259
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-11-24 19:19:28 +0600

    Shakib_Dev

--- front-end/src/Components/TopDoctors.jsx
+++ front-end/src/Components/TopDoctors.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { doctors } from '../assets/assets_frontend/assets'
 
 const TopDoctors = () => {
+
+
+    
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium' >Top Doctors to Book</h1>
