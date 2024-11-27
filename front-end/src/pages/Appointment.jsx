4d61f58ad2c244f2939b942e59491c96beb3c5ad
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-11-27 22:18:02 +0600

    Shakib_Dev

--- front-end/src/pages/Appointment.jsx
+++ front-end/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets_frontend/assets';
+import RelatedDoctors from '../Components/RelatedDoctors';
 
 const Appointment = () => {
   const { docId } = useParams();
@@ -142,6 +143,9 @@ const Appointment = () => {
         </div>
         <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
       </div>
+        
+        <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
+
     </div>
   );
 };
