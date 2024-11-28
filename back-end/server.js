ee59a9a5fa52be419f86c76eb76d97b07766be34
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-11-28 20:01:57 +0600

    connected database

--- back-end/server.js
+++ back-end/server.js
@@ -1,16 +1,20 @@
-import express from 'express'; 
-import cors from 'cors'
-import 'dotenv/config'
+import express from 'express'
+import cors from'cors'
+import 'dotenv/config.js'
+import connectDB from './config/mongodb.js';
 
 
-const app = express(); 
+const app = express()
 const port = process.env.PORT || 5000
+connectDB()
 
 app.use(express.json())
 app.use(cors())
 
+
 app.get('/',(req,res)=>{
-    res.send('API Working')
+  res.send('API Working')
 })
 
-app.listen(port,()=>console.log("Server Started", port))
+
+app.listen(port , ()=>console.log("server Started",port))
