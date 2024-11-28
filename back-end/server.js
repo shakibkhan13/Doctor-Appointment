faa75e0f67831fd5e822b20942670a7f43927eeb
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-11-28 20:30:22 +0600

    Shakib_Dev

--- back-end/server.js
+++ back-end/server.js
@@ -2,11 +2,12 @@ import express from 'express'
 import cors from'cors'
 import 'dotenv/config.js'
 import connectDB from './config/mongodb.js';
-
+import connectCloudinary from './config/cloudinary.js';
 
 const app = express()
 const port = process.env.PORT || 5000
 connectDB()
+connectCloudinary()
 
 app.use(express.json())
 app.use(cors())
