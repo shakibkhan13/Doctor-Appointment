28c43a475a789b2207ba93db047db985a96e940e
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-12-07 01:20:51 +0600

    Update

--- back-end/server.js
+++ back-end/server.js
@@ -7,7 +7,7 @@ import adminRouter from "./routes/adminRoutes.js";
 
 //app config
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 connectDB();
 // connectCloudinary()
 
