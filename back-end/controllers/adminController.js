51db372857a1451320e1c89e2cf6796f30e41cdc
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-12-22 03:25:05 +0600

    Create Admin pannel

--- back-end/controllers/adminController.js
+++ back-end/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from 'validator';
 import bcrypt from 'bcrypt';
 import { v2 as cloudinary } from 'cloudinary';
 import doctorsModel from '../models/doctorsModel.js'; 
+import jwt from 'jsonwebtoken'
 
 const addDoctor = async (req, res) => {
   try {
@@ -74,4 +75,25 @@ const addDoctor = async (req, res) => {
   }
 };
 
-export { addDoctor };
+// admin login 
+const loginAdmin = async (req, res)=>{
+  try {
+    const {email, password} = req.body ; 
+
+
+    if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+      const token = jwt.sign(email+password, process.env.JWT_SECRET)
+      res.json({success:true,token})
+
+    }else{
+      res.json({success:false, message:"Invalid credentials"})
+    }
+
+  } catch (error) {
+    console.log(error)
+    res.json({success:false, message:error.message})
+  }
+}
+
+
+export { addDoctor, loginAdmin };
