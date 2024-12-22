377a745d0440d2509d7fabaa2cb6f8184f8918e4
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-12-22 11:31:43 +0600

    Shakib_dev

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,12 +1,39 @@
-import React, { useState } from 'react'
-import { assets } from '../assets/assets_admin/assets'
+import { useContext, useState } from 'react';
+import { AdminContext } from '../context/AdminContext';
+import axios from 'axios';
+import { toast } from 'react-toastify'
 
 const Login = () => {
+  const [state, setState] = useState('Admin');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const [state, setState] = useState('Admin')
+  const { setAToken, backendUrl } = useContext(AdminContext);
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      if (state === 'Admin') {
+        const { data } = await axios.post(backendUrl + '/api/admin/login', { email, password });
+        if (data.success) {
+          localStorage.setItem('aToken' , data.token)
+          setAToken(data.token);
+        }else{
+          toast.error(data.message)
+        }
+
+
+      } else {
+        
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
         <p className='text-2xl font-semibold m-auto'>
           <span className='text-primary'> {state} </span>
@@ -15,31 +42,42 @@ const Login = () => {
 
         <div className='w-full'>
           <p>Email : </p>
-          <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className='border border-[#DADADA] rounded w-full p-2 mt-1'
+            type="email"
+            required
+          />
         </div>
 
         <div className='w-full'>
           <p>Password : </p>
-          <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            className='border border-[#DADADA] rounded w-full p-2 mt-1'
+            type="password"
+            required
+          />
         </div>
 
         <button className='bg-primary text-white w-full py-2 rounded-md text-base'>Login</button>
-        {
-          state === 'Admin'
-            ? <p>
-              Doctor Login?&nbsp; 
-              <span className='text-primary underline cursor-pointer' onClick={()=>setState('Doctor')}>Click here</span>
-            </p>
-            :
-            <p>
-              Admin Login?&nbsp;
-              <span className='text-primary underline cursor-pointer' onClick={()=>setState('Admin')}>Click here</span>
-            </p>
-        }
 
+        {state === 'Admin' ? (
+          <p>
+            Doctor Login?&nbsp;
+            <span className='text-primary underline cursor-pointer' onClick={() => setState('Doctor')}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Admin Login?&nbsp;
+            <span className='text-primary underline cursor-pointer' onClick={() => setState('Admin')}>Click here</span>
+          </p>
+        )}
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default Login; 
+export default Login;
