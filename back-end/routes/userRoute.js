77385fd74c0c3ed750cd34a44faea0bcc362dc7b
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-12-24 23:00:16 +0600

    profile update

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -1,11 +1,13 @@
-import express from 'express'; 
-import { registerUser , loginUser } from '../controllers/userControler.js';
+import express from "express";
+import { registerUser, loginUser, getProfile, updateProfile } from "../controllers/userControler.js";
+import upload from '../middlewares/multer.js'; 
+import authUser from "../middlewares/authUser.js";
 
+const userRouter = express.Router();
 
-const userRoute = express.Router()
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post("/update-profile", upload.single('image'), authUser, updateProfile);
 
-userRoute.post('/register' , registerUser)
-userRoute.post('/login' , loginUser)
-
-
-export default userRoute ; 
+export default userRouter;
