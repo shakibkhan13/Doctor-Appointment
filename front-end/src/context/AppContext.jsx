71e407bf384cef8410ccc8b95e3829e8a44c1b34
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-12-24 15:08:32 +0600

    User Auth Complete

--- front-end/src/context/AppContext.jsx
+++ front-end/src/context/AppContext.jsx
@@ -10,10 +10,8 @@ const AppContextProvider = (props) => {
     const currencySymbol = 'BDT'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [doctors, setDoctors] = useState([])
+    const [token , setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
 
-    const value = {
-        doctors, currencySymbol
-    }
 
     const getDoctorsData = async () => {
         try {
@@ -29,6 +27,11 @@ const AppContextProvider = (props) => {
             console.log(error)
             toast.error(error.message)
         }
+    }
+
+    const value = {
+        doctors, currencySymbol , token, setToken, 
+        backendUrl
     }
 
     useEffect(()=>{
