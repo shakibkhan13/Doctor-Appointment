896215a1b3be8d41cfc9dcf0fa68995e18d1172a
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-12-25 02:39:50 +0600

    Appointment page update

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser, getProfile, updateProfile } from "../controllers/userControler.js";
+import { registerUser, loginUser, getProfile, updateProfile , bookAppointment } from "../controllers/userControler.js";
 import upload from '../middlewares/multer.js'; 
 import authUser from "../middlewares/authUser.js";
 
@@ -9,5 +9,6 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/get-profile", authUser, getProfile);
 userRouter.post("/update-profile", upload.single('image'), authUser, updateProfile);
+userRouter.post("/book-appointment", authUser , bookAppointment)
 
 export default userRouter;
